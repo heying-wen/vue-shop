36fed35c7e85501cd490d03b2c90009f585ce09c
heying-wen <email@example.com>
2020-04-17 17:01:12 +0800

    feat:创建版本

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="head">
+    <span class="iconfont back" onclick="toBack()">&#xe604;</span>
+    <span class="subject">{{title}}</span> 
+</div>
+</template>
+<script>
+export default {
+    props:{
+    }
+}
+</script>
+<style lang="scss" scopeds>
+@import "~@assets/cscc/global";
+</style> 
